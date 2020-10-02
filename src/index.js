95dfd16b96c7883a54c6d51793d3cf5c9f7e1854
skamok <s.korshankov@belarusneft.beloil.by>
2020-10-02 08:14:41 +0300

    init: add solve to Revert Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = [];
+    let str ='';
+    str = n.toString();
+    for (let i = 0; i < str.length; i++) {
+      arr.push(str[i]);
+    }
+    return Number.parseInt(arr.reverse().join(''));
 }
